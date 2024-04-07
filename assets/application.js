8d4f17c443d1c1b20992349638ef1efd52700359
duc.nguyenvu <duc.nguyenvu@spiraledge.com>
2024-04-08 00:16:30 +0700

    Add recommendations product and  Predictive Search API

--- assets/application.js
+++ assets/application.js
@@ -156,3 +156,50 @@ function update_cart() {
     .then((data) => document.getElementById("numberOfCartItems").innerHTML = data.items.length)
     .catch((err) => console.error(err));
 }
+
+
+var predictiveSearchInput = document.getElementById('searchInputField');
+var timer;
+
+var offcanvasSearch = document.getElementById('offcanvasSearchResult');
+var bsOffcanvas = new bootstrap.Offcanvas(offcanvasSearch);
+
+if(predictiveSearchInput != null) {
+    predictiveSearchInput.addEventListener('input', function(e) {
+
+        clearTimeout(timer);
+
+        if(predictiveSearchInput.value) {
+            timer = setTimeout(fetchPredictiveSearch, 300);
+        }
+
+
+    });
+}
+
+function fetchPredictiveSearch() {
+    fetch(`/search/suggest.json?q=${predictiveSearchInput.value}&resources[type]=product`)
+    .then(resp => resp.json())
+    .then(data => { 
+        console.log(data);
+
+        var products = data.resources.results.products;
+
+        document.getElementById('search_results_body').innerHTML = '';
+
+        products.forEach(function(product, index) {
+            document.getElementById('search_results_body').innerHTML += `
+                <div class="card" style="width: 19rem;">
+                    <a href = "${product.url}" title = "${product.title}">
+                        <img src="${product.image}" class="card-img-top w-100">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">$${product.price}</p>
+                    </div>
+                </div>
+            `
+        });
+        bsOffcanvas.show();
+    });
+}
